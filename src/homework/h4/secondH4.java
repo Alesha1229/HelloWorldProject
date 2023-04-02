1aea72f4ef0db40e3f3535050142aedbda20eb43
Alesha1229 <ale05xi@gmail.com>
2023-04-02 14:41:00 +0300

    Homework 4 is complete Thats perfect!!!

---
+++ src/homework/h4/secondH4.java
@@ -0,0 +1,29 @@
+package homework.h4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class secondH4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("enter the number to destroy");
+
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(10);
+            if (arr[i] == num) {
+                arr[i]=0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
