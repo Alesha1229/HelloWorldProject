51ce3eb58fc1a2aa134289122ceef7d3bbdb3529
Alesha1229 <ale05xi@gmail.com>
2023-04-04 20:37:16 +0300

    homework 5 is complete!!!

---
+++ src/homework/h5/FourthTask.java
@@ -0,0 +1,25 @@
+package homework.h5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class FourthTask {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        int sum = 0;
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = random.nextInt(5);
+                sum = sum + arr[i][j];
+
+
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+        System.out.println(sum);
+
+
+    }
+}
+
