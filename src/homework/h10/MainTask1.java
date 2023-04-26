ab559b22b5ec84329847463819fadc0e0ae6f1bf
Alesha1229 <ale05xi@gmail.com>
2023-04-26 14:01:46 +0300

    homework 10 in process

--- src/homework/h10/MainTask1.java
+++ src/homework/h10/MainTask1.java
@@ -3,55 +3,12 @@ package homework.h10;
 import java.util.Scanner;
 
 public class MainTask1 {
-    static String format = "1234-abc-5678-def-1a2b";
-
-
-
-
-
     public static void main(String[] args) {
-        System.out.println("Напишите номер док-та в таком формате: xxxx-yyy-xxxx-yyy-xyxy");
-        System.out.println("x-цифры, y-буквы");
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder format = new StringBuilder();
 
-        System.out.println("xxxx:");
-        String first4x = scanner.nextLine();
-        format.append(first4x);
-
-        format.append("-");
-
-        System.out.println("yyy:");
-        String first3y = scanner.nextLine();
-        format.append(first3y);
-
-        format.append("-");
-
-        System.out.println("xxxx:");
-        String second4x = scanner.nextLine();
-        format.append(second4x);
-
-        format.append("-");
-
-        System.out.println("yyy:");
-        String second3y = scanner.nextLine();
-        format.append(second3y);
-
-        format.append("-");
-
-        System.out.println("xyxy:");
-        String xyxy = scanner.nextLine();
-        format.append(xyxy);
-
-        System.out.println(format);
+        NumberGenerate document1 = new NumberGenerate("1234","abc","5678","def","1a2b");
 
 
-
-
-        MethodsString.task1(format);
-
-
-
+        //MethodsString.task1();
 
 
     }
--- src/homework/h10/MethodsString.java
+++ src/homework/h10/MethodsString.java
@@ -1,15 +1,22 @@
 package homework.h10;
 
 public class MethodsString {
-    public static void task1(StringBuilder format){
+
+    public static void task1(){
         System.out.println("- Вывести на экран в одну строку два первых блока по 4 цифры.");
-        System.out.print(format.substring(0,4));
-        System.out.println(format.substring(9,13));
+        System.out.print(NumberGenerate.getFormat.substring(0,4));
+        System.out.println(NumberGenerate.format.substring(9,13));
     }
-    public static void task2(StringBuilder format) {
+    public static void task2() {
         System.out.println("- Вывести на экран номер документа, но блоки из трех букв заменить на *** (каждая буква заменятся на *).");
 
+
+
+
+
+
     }
+
 
 
 
