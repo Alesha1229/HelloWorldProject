e37e2c9b1f61cd969eac5ee64fc1568580f169c3
Alesha1229 <ale05xi@gmail.com>
2023-04-26 19:10:52 +0300

    homework 10 in process

--- src/homework/h10/MainTask1.java
+++ src/homework/h10/MainTask1.java
@@ -5,10 +5,49 @@ import java.util.Scanner;
 public class MainTask1 {
     public static void main(String[] args) {
 
-        NumberGenerate document1 = new NumberGenerate("1234","abc","5678","def","1a2b");
 
 
-        //MethodsString.task1();
+
+              System.out.println("Напишите номер док-та в таком формате: xxxx-yyy-xxxx-yyy-xyxy");
+        System.out.println("x-цифры, y-буквы");
+        StringBuilder format = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("xxxx:");
+        //String first4x = scanner.nextLine();
+       String first4x = "1234";
+        format.append(first4x);
+
+        format.append("-");
+
+       System.out.println("yyy:");
+       //String first3y = scanner.nextLine();
+       String  first3y = "abc";
+       format.append(first3y);
+
+       format.append("-");
+
+       System.out.println("xxxx:");
+       //String second4x = scanner.nextLine();
+       String second4x = "5678";
+       format.append(second4x);
+
+       format.append("-");
+
+       System.out.println("yyy:");
+       //String second3y = scanner.nextLine();
+       String second3y = "def";
+       format.append(second3y);
+
+       format.append("-");
+
+       System.out.println("xyxy:");
+       //String xyxy = scanner.nextLine();
+       String xyxy = "1a2b";
+       format.append(xyxy);
+
+       System.out.println(format);
 
 
     }
--- src/homework/h10/NumberGenerate.java
+++ src/homework/h10/NumberGenerate.java
@@ -9,63 +9,9 @@ public class NumberGenerate {
    private String yyy2;
    private String xyxy;
 
-    public NumberGenerate(String xxxx1, String yyy1, String xxxx2, String yyy2, String xyxy, StringBuilder format) {
-        this.xxxx1 = xxxx1;
-        this.yyy1 = yyy1;
-        this.xxxx2 = xxxx2;
-        this.yyy2 = yyy2;
-        this.xyxy = xyxy;
-        format.append(xxxx1);
-        format.append(yyy1);
-        format.append(xxxx2);
-        format.append(yyy2);
-        format.append(xyxy);
-        System.out.println(format);
 
-    }
 
-    //    public static void GenerateFormat(){
-//
-//        System.out.println("Напишите номер док-та в таком формате: xxxx-yyy-xxxx-yyy-xyxy");
-//        System.out.println("x-цифры, y-буквы");
-//        StringBuilder format = new StringBuilder();
-//        Scanner scanner = new Scanner(System.in);
-//
-//
-//        System.out.println("xxxx:");
-//        //String first4x = scanner.nextLine();
-//        String first4x = "1234";
-//        format.append(first4x);
-//
-//        format.append("-");
-//
-//        System.out.println("yyy:");
-//        //String first3y = scanner.nextLine();
-//        String  first3y = "abc";
-//        format.append(first3y);
-//
-//        format.append("-");
-//
-//        System.out.println("xxxx:");
-//        //String second4x = scanner.nextLine();
-//        String second4x = "5678";
-//        format.append(second4x);
-//
-//        format.append("-");
-//
-//        System.out.println("yyy:");
-//        //String second3y = scanner.nextLine();
-//        String second3y = "def";
-//        format.append(second3y);
-//
-//        format.append("-");
-//
-//        System.out.println("xyxy:");
-//        //String xyxy = scanner.nextLine();
-//        String xyxy = "1a2b";
-//        format.append(xyxy);
-//
-//        System.out.println(format);
-//    }
+
+
 
 }
