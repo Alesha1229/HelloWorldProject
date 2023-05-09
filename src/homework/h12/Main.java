3cb00e2f9ee3dc5557ae6f31e7c6b462619f9f19
Alesha1229 <ale05xi@gmail.com>
2023-05-09 23:12:09 +0300

    homework 12 process

---
+++ src/homework/h12/Main.java
@@ -0,0 +1,9 @@
+package homework.h12;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        DocCreator.CreateDoc("docum1234567890","docum0987654321","docum1234567890");
+    }
+}
